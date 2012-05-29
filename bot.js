9ace1882111a702941f4a1d7061e734c08c5654d
Vendan <andyleap@andyleap.net>
2012-05-29 15:15:49 +0000

    testing

--- bot.js
+++ bot.js
@@ -5,17 +5,12 @@ var client = new ircLib.Client('irc.lavishsoft.com', 'ComBot', {
     channels: ['#combot'],
 });
 
-client.addListener('message', function (from, to, message) {
-
-});
-
-
 var app = express.createServer();
 app.use(express.bodyParser());
 
 app.post('/', function(req, res){
     var push = JSON.parse(req.body.payload);
-    client.say('#combot', push.pusher.name + "pushed commits");
+    client.say('#combot', push.pusher.name + " pushed commits");
 });
 
 app.listen(process.env.PORT, '0.0.0.0');
