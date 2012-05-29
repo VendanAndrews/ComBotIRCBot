fcd287c474564ae7a1723bf84efd41ad78fa761a
Vendan <andyleap@andyleap.net>
2012-05-29 15:02:26 +0000

    testing

--- bot.js
+++ bot.js
@@ -1,20 +1,20 @@
 var ircLib = require('irc');
 var express = require('express');
 
-var client = new ircLib.Client('irc.lavishsoft.com', 'ComBot', {
+/*var client = new ircLib.Client('irc.lavishsoft.com', 'ComBot', {
     channels: ['#combot'],
 });
 
 client.addListener('message', function (from, to, message) {
 
 });
-
+*/
 
 var app = express.createServer();
 app.use(express.bodyParser());
 
 app.post('/', function(req, res){
-    console.log(req.body.payload);
+    console.log(JSON.parse(req.body.payload));
 });
 
 app.listen(process.env.PORT, '0.0.0.0');
